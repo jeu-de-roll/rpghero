9a5e1da0464b91e9274b07f385761bcde90e52ea
Nekkiji <72854349+Nekkiji@users.noreply.github.com>
2024-03-27 19:37:17 -0400

    Add sharefile Page

--- app/src/main/java/com/example/rpghero/MainActivity.kt
+++ app/src/main/java/com/example/rpghero/MainActivity.kt
@@ -2,16 +2,41 @@ package com.example.rpghero
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement.Center
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.res.painterResource
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreHoriz
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.*
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
 import com.example.rpghero.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Arrangement.Horizontal
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : AppCompatActivity() {
 
@@ -50,5 +75,121 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
+
+
+@Preview
+@Composable
+fun ShareFilePage()
+{
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 16.dp, horizontal = 16.dp))
+    {
+        //BAR
+        Text("YOUR FILES", fontSize = 34.sp)
+        Row (modifier = Modifier
+            .weight(1f)
+            .padding(vertical = 16.dp)){
+            FilesGrid(arrayOf("Image 1", "Ma carte.pdf", "Pnj artwork", "Mp3 404", "Meme trop marrant", "Machin", "Chose", "Machin", "Chose", "Machin", "Chose", "Machin", "Chose", "Machin", "Chose"))
+            //Slider(value = , onValueChange = )
+        }
+        Button(onClick = { /*TODO*/ })
+        {
+            Text("Import File", fontSize = 34.sp)
+        }
+    }
+}
+
+@Composable
+fun FilesGrid (files : Array<String>)
+{
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(minSize = 128.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(files)
+        {
+            file -> File(file)
+        }
+    }
+}
+
+@Composable
+fun File (name : String)
+{
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(id = R.drawable.placeholder),
+            contentDescription = null)
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(text = name, fontSize = 16.sp)
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            )
+            IconButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.size(24.dp)
+            ) {
+                Icon(
+                    Icons.Filled.MoreHoriz,
+                    contentDescription = null
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun sharePopup()
+{
+    Column (
+        modifier = Modifier.width(150.dp)
+    ){
+        TextButton (
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.Gray),
+            contentPadding = PaddingValues(0.dp),
+
+        ) {
+            Icon(
+                Icons.Filled.MoreHoriz,
+                contentDescription = null)
+            Text("Share to a player", fontSize = 14.sp)
+        }
+        Button (
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Icon(
+                Icons.Filled.MoreHoriz,
+                contentDescription = null)
+            Text("Share to all players", fontSize = 14.sp)
+        }
+        Button (
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Icon(
+                Icons.Filled.MoreHoriz,
+                contentDescription = null)
+            Text("Delete", fontSize = 14.sp)
+        }
     }
 }
