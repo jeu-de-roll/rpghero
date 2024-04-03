51203f5f218be1d2fefce80e91d07e154f10f848
Nekkiji <72854349+Nekkiji@users.noreply.github.com>
2024-04-03 17:23:01 -0400

    Add Roll d20 on first page

--- app/src/main/java/com/example/rpghero/MainActivity.kt
+++ app/src/main/java/com/example/rpghero/MainActivity.kt
@@ -4,8 +4,13 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -16,9 +21,16 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.NavHost
@@ -38,6 +50,7 @@ import com.example.rpghero.room.Chronics
 import com.example.rpghero.room.ChronicsListMenu
 import com.example.rpghero.room.RoomTopMenu
 import com.example.rpghero.room.SessionSettingsMenu
+import kotlin.random.Random
 
 
 class MainActivity : ComponentActivity() {
@@ -175,7 +188,30 @@ fun HomeScreen(navigateToOpenScreen: () -> Unit, navigateToJoinScreen: () -> Uni
         floatingActionButton = {
             FloatingActionButtonDice()
         },
-        content = { }
+        content = {
+            var clicked by remember { mutableStateOf(false) }
+            var dice by remember { mutableIntStateOf(404) }
+            val rand = Random
+            Column(modifier = Modifier
+                .fillMaxSize()
+                .clickable
+                {
+                    dice = rand.nextInt(20) + 1
+                    clicked = true
+                },
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                if (clicked) {
+                    Text(text = dice.toString(), style = MaterialTheme.typography.displayLarge)
+                }
+                else {
+                    Text("Tap on Screen to roll for initiative",
+                        style = MaterialTheme.typography.displayLarge,
+                        textAlign = TextAlign.Center)
+                }
+            }
+        }
     )
 }
 
