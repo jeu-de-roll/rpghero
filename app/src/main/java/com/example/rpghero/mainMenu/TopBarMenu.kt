9fab54166556b001f4733c8a5a13b20df7b1e0c7
Enrion <theeosimon@gmail.com>
2024-04-10 18:57:17 -0400

    Changing text in top bar buttons

--- app/src/main/java/com/example/rpghero/mainMenu/TopBarMenu.kt
+++ app/src/main/java/com/example/rpghero/mainMenu/TopBarMenu.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -19,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.times
 
@@ -35,9 +37,9 @@ fun TopBarMenu(
         horizontalArrangement = Arrangement.SpaceEvenly,
     )
     {
-        TextButtonBox(text = "OPEN", onClickEvent = navigateToOpenScreen)
-        TextButtonBox(text = "JOIN", onClickEvent = navigateToJoinScreen)
-        TextButtonBox(text = "CREATE", onClickEvent = navigateToCreateScreen)
+        TextButtonBox(text = "SESSION" + "\n" + "LIST", onClickEvent = navigateToOpenScreen)
+        TextButtonBox(text = "JOIN" + "\n" + "SESSION", onClickEvent = navigateToJoinScreen)
+        TextButtonBox(text = "CREATE" + "\n" + "SESSION", onClickEvent = navigateToCreateScreen)
         IconButtonBox(icon = Icons.Rounded.Settings, onClickEvent = navigateToParamScreen)
     }
 }
@@ -59,8 +61,8 @@ fun TextButtonBox(text : String, onClickEvent: () -> Unit)
             }
     ) {
         Text(
-            modifier = Modifier
-                .align(Alignment.Center),
+            modifier = Modifier.align(Alignment.Center),
+            textAlign = TextAlign.Center,
             text = text,
             style = MaterialTheme.typography.bodySmall,
         )
