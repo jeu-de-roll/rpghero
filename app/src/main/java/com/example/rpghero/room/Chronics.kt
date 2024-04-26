5919fd81f4f1de8e850caaca126d010595d03578
Enrion <theeosimon@gmail.com>
2024-04-26 14:40:44 -0400

    Making textfield editable

--- app/src/main/java/com/example/rpghero/room/Chronics.kt
+++ app/src/main/java/com/example/rpghero/room/Chronics.kt
@@ -2,11 +2,17 @@ package com.example.rpghero.room
 
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
 fun Chronics () {
+    var text by remember { mutableStateOf("") }
+
     TextField(
-        value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-        onValueChange = { }
+        value = text,
+        onValueChange = { text = it }
     )
 }
